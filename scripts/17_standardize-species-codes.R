201418b82fba6bf1028d57e1bf1300285ae13b37
Lia Ossanna <lossanna@arizona.edu>
2026-07-09 15:21:17 -0600

    Fix 17.R script so that all NameCodes have assignments (there were some not present in tblNationalPlants); also drop non-plants.

--- scripts/17_standardize-species-codes.R
+++ scripts/17_standardize-species-codes.R
@@ -1,16 +1,19 @@
 # Created: 2026-06-30
-# Updated: 2026-07-06
+# Updated: 2026-07-09
 
 # Purpose: Standardize plant species names/codes, and assign cover values for all matched data.
 #   Include codes in geospecies with no LPI data (these species were found during the
 #   species inventory but not measured on the transect for LPI). Assign these species a 
 #   cover value of 0.5% to be able to calculate diversity later on.
 
+# Rows with NA for Code in geoindicators.csv or NA for Species in geospecies.csv were dropped.
+
 # devtools::install_github("landscape-data-commons/trex", build_vignettes = TRUE)
 
 library(tidyverse)
 library(foreign)
 library(terradactyl)
+library(readxl)
 
 # Load data ---------------------------------------------------------------
 
@@ -30,26 +33,26 @@ all.matched <- bind_rows(mget(ls(pattern = "\\.matched$")),
 # List of relevant species
 matched.species <- geospecies |> 
   filter(PrimaryKey %in% all.matched$PrimaryKey) |> 
-  select(Species, ScientificName, Duration, Woody, Lifeform) |> 
+  select(Species, ScientificName, GrowthHabit, GrowthHabitSub, Duration) |> 
   distinct(.keep_all = TRUE)
 
 
 
-# Data wrangling for National Plants DB -----------------------------------
+# Narrow down National Plants DB ------------------------------------------
 
 # Narrow down to NameCodes that appear in matched.species
 plants.db <- plants.db.raw |> 
   filter(NameCode %in% matched.species$Species) |> 
   rename(CurrentPLANTSCode = CurrentPLA,
-         GrowthHabitat = GrowthHabi,
-         GrowthHabitat_sub = GrowthHa_1)
+         GrowthHabit = GrowthHabi,
+         GrowthHabitSub = GrowthHa_1)
 
 
-## Create crosswalk -------------------------------------------------------
+# Create crosswalk --------------------------------------------------------
 
 # Cols for grouping
 crosswalk1 <- plants.db |> 
-  select(NameCode, Scientific, CurrentPLANTSCode, GrowthHabitat, GrowthHabitat_sub, Duration,
+  select(NameCode, Scientific, CurrentPLANTSCode, GrowthHabit, GrowthHabitSub, Duration,
          Nonnative, Invasive) |> 
   arrange(NameCode) |> 
   arrange(CurrentPLANTSCode) 
@@ -63,7 +66,7 @@ multiple.codes <- crosswalk1 |>
   filter(CurrentPLANTSCode %in% multiple.codes.codes$CurrentPLANTSCode)
 
 
-### Example of multiple CurrentPLANTSCode: Lupin --------------------------
+## Example of multiple CurrentPLANTSCode: Lupin ---------------------------
 
 # This section is to demonstrate the situation of multiple CurrentPLANTSCode values
 #   that have different NameCodes, and how to handle them.
@@ -85,7 +88,7 @@ lupin
 #   not indentified to the species level, and should remain as a separate group.
 
 
-### Handle scientific names with parentheses ------------------------------
+## Handle scientific names with parentheses -------------------------------
 
 # Inspect scientific names with parentheses
 sci.parenth <- crosswalk1 |> 
@@ -134,12 +137,17 @@ crosswalk3 <- crosswalk2 |>
   bind_rows(problem.codes.fixed)
 
 
-# Save final version
-crosswalk <- crosswalk3
+
+## Check for instances of no CurrentPLANTSCode ----------------------------
+
+crosswalk3 |> 
+  filter(is.na(CurrentPLANTSCode)) # no instances
 
 
 
-# Apply crosswalk fix -----------------------------------------------------
+## Check LPI and geospecies data for missing codes ------------------------
+
+### LPI -------------------------------------------------------------------
 
 # Filter LPI data for primary keys in all.matched
 all.matched.lpi <- lpi |> 
@@ -148,6 +156,119 @@ all.matched.lpi <- lpi |>
 # Rename Code col as NameCode
 all.matched.lpi <- all.matched.lpi |> 
   rename(NameCode = Code)
+
+# Examine NA codes
+all.matched.lpi.na <- all.matched.lpi |> 
+  filter(is.na(NameCode))
+
+#   Remove NA codes
+all.matched.lpi <- all.matched.lpi |> 
+  filter(!is.na(NameCode))
+
+# Names not in crosswalk
+setdiff(all.matched.lpi$NameCode, crosswalk3$NameCode)
+
+
+### geospecies ------------------------------------------------------------
+
+# Filter geospecies for primary keys in all.matched
+all.matched.geospecies <- geospecies |> 
+  filter(PrimaryKey %in% all.matched$PrimaryKey)
+
+# Rename Species col to NameCode
+all.matched.geospecies <- all.matched.geospecies |> 
+  rename(NameCode = Species) 
+
+# Look for NA codes
+all.matched.geospecies |> 
+  filter(is.na(NameCode)) # no instances
+
+# Codes not in crosswalk
+setdiff(all.matched.geospecies$NameCode, crosswalk3$NameCode)
+
+
+### Combine ---------------------------------------------------------------
+
+codes.missing <- data.frame(
+  NameCode = union(setdiff(all.matched.lpi$NameCode, crosswalk3$NameCode),
+                   setdiff(all.matched.geospecies$NameCode, crosswalk3$NameCode)),
+  Scientific = NA,
+  CurrentPLANTSCode = NA,
+  GrowthHabit = NA,
+  GrowthHabitSub = NA,
+  Duration = NA,
+  Nonnative = NA,
+  Invasive = NA
+)
+
+codes.missing <- codes.missing |> 
+  arrange(NameCode)
+
+# OUTPUT: List of codes missing from crosswalk
+write_csv(codes.missing,
+          file = "data/data-wrangling-intermediate/17a_output1_codes-missing-from-plants-database.csv",
+          na = "")
+
+unique(crosswalk3$GrowthHabit)
+unique(crosswalk3$GrowthHabitSub)
+unique(crosswalk3$Duration)
+unique(crosswalk3$Nonnative)
+unique(crosswalk3$Invasive)
+
+
+# EDITED: List of codes missing from crosswalk with columns assigned
+#   Columns: Scientific, CurrentPLANTSCode, GrowthHabit, GrowthHabitSub, Duration, Nonnative, Invasive
+#   For codes that are in USDA Plants Database, add info; check with crosswalk so other
+#     grouping cols match if the species (as CurrentPLANTSCode) already exists.
+#   1- and 2-letter codes are dropped because they likely refer to non-plants (except
+#     AF, PF, AG, PG, SH, TR, or a variation)
+codes.missing.fixed <- read_xlsx("data/data-wrangling-intermediate/17b_edited1_codes-missing-from-plants-database.xlsx")
+
+
+# Add fix to crosswalk
+crosswalk4 <- crosswalk3 |> 
+  bind_rows(codes.missing.fixed)
+
+
+## Create GrowthHabitSub2 column ------------------------------------------
+
+# GrowthHabitSub values
+count(crosswalk4, GrowthHabitSub)
+
+# Collapse subshrub, succulent, and tree into shrub category
+crosswalk5 <- crosswalk4 |> 
+  mutate(GrowthHabitSub2 = case_when(
+    GrowthHabitSub == "SubShrub" ~ "Shrub",
+    GrowthHabitSub == "Succulent" ~ "Shrub",
+    GrowthHabitSub == "Tree" ~ "Shrub",
+    TRUE ~ GrowthHabitSub
+  ))
+
+
+## Check for conflicting info ---------------------------------------------
+
+# Multiple rows for same CurrentPLANTSCode
+multiple.check.codes <- count(crosswalk5, CurrentPLANTSCode) |> 
+  filter(n > 1)
+
+multiple.check <- crosswalk5 |> 
+  filter(CurrentPLANTSCode %in% multiple.check.codes$CurrentPLANTSCode) |> 
+  arrange(CurrentPLANTSCode)
+
+# Remove NameCode and Scientific and collapse
+multiple.check.collapsed <- multiple.check |> 
+  select(-NameCode, -Scientific) |> 
+  distinct(.keep_all = TRUE)
+
+# Check for matching lengths
+length(unique(multiple.check$CurrentPLANTSCode)) == nrow(multiple.check.collapsed)
+
+
+# Save final crosswalk version
+crosswalk <- crosswalk5
+
+
+# Apply crosswalk fix to LPI ----------------------------------------------
 
 # Join crosswalk
 all.matched.lpi.joined <- all.matched.lpi |> 
@@ -183,11 +304,12 @@ lpi.recalculate1 <- pct_cover(lpi_tall = all.matched.lpi.joined,
                               hit = "any",
                               indicator_variables = "CurrentPLANTSCode")
 
-# Drop 0s and rename cols
+# Drop 0s and non-plants and rename cols
 lpi.recalculate2 <- lpi.recalculate1 |> 
   filter(percent > 0) |> 
   rename(CurrentPLANTSCode = indicator,
-         Cover_AH = percent) 
+         Cover_AH = percent) |> 
+  filter(CurrentPLANTSCode != "Drop") 
 
 # Save final version
 lpi.recalculate <- lpi.recalculate2
@@ -196,24 +318,16 @@ lpi.recalculate <- lpi.recalculate2
 
 # Create cover table for non-LPI species ----------------------------------
 
-# Apply crosswalk fix
-#   Filter geospecies for primary keys in all.matched
-all.matched.geospecies <- geospecies |> 
-  filter(PrimaryKey %in% all.matched$PrimaryKey)
-
-#   Rename Species col to NameCode
-all.matched.geospecies <- all.matched.geospecies |> 
-  rename(NameCode = Species) 
-
+# Apply crosswalk fix to geospecies
 #   Join crosswalk
 all.matched.geospecies.joined <- all.matched.geospecies |> 
   left_join(crosswalk) 
 
-#   Collapse into groups via CurrentPLANTSCode
+#   Collapse into groups via CurrentPLANTSCode and remove non-plants
 all.matched.geospecies.fixed <- all.matched.geospecies.joined |> 
   select(PrimaryKey, SpeciesCover_AH, CurrentPLANTSCode) |> 
-  distinct(.keep_all = TRUE)
-
+  distinct(.keep_all = TRUE) |> 
+  filter(CurrentPLANTSCode != "Drop")
 
 # Narrow down to only plants with NA species cover (not included in LPI)
 all.matched.nonlpi <- all.matched.geospecies.fixed |> 
@@ -241,6 +355,10 @@ species.info.fixed <- crosswalk |>
 cover <- cover |> 
   left_join(species.info.fixed)
 
+# Check for NA codes
+cover |> 
+  filter(is.na(CurrentPLANTSCode))
+
 
 # Add to species cover data to all.matched --------------------------------
 
@@ -250,7 +368,6 @@ all.matched.cover <- all.matched |>
 #   keys in all.matched will have multiple rows in cover (many species), and rows in
 #   cover might match multiple rows in all.matched because there are multiple instances of
 #   the same primary key because the same point might be used in multiple models.
-
 
 
 # Write to CSV ------------------------------------------------------------
